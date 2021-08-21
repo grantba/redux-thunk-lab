a5366a11e7c1c15514e5a8ec020dbcc414b73454
grantba <grant.blaire@gmail.com>
2021-08-20 23:58:04 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import CatList from './CatList'
+import {fetchCats} from './actions/catActions'
 
 class App extends Component {   
+
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
   
   render() {
+    console.log(this.props.catPics)
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
-      </div>
+        {this.props.loading === true ? <h2>Loading...</h2> : null}
+        {this.props.catPics !== undefined ? <CatList catPics={this.props.catPics} loading={this.props.loading}/> : null}
+      </div> 
     );
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchCats: () => dispatch(fetchCats())
+//   }
+// }
+
+export default connect(mapStateToProps, {fetchCats})(App)
+
+// handleLoading = () => {
+//   console.log(this.props.loading)
+//   if(this.props.loading) {
+//     return <div>Loading...</div>
+//   } else {
+//     return <CatList catPics={this.props.catPics} />
+//   }
+// }
+
+// render() {
+//   return (
+//     <div className="App">
+//       <h1>CatBook</h1>
+//       {this.handleLoading()}
+//     </div>
+//   );
+// }
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,27 @@
 // write your CatList component here
+import React from 'react'
+
+export default function CatList(props) {
+
+    let getCats = props.catPics.map(cat => {return <img key={cat.id} src={cat.url} alt={cat.id}/>})
+
+    return (
+        <div>
+            {getCats}
+        </div>
+    )
+}
+
+// class CatList extends React.Component {
+//     listCats = () => {
+//       return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           {this.listCats()}
+//         </div>
+//       )
+//     }
+//   }
